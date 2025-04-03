82f0b0a002131878fd1858058de056c0162be0d7
Chidarengan <lucasdesamartins@hotmail.com>
2025-04-03 09:49:51 -0400

    faltou o link

--- cursoCFB/aula02/aula02.ts
+++ cursoCFB/aula02/aula02.ts
@@ -4,4 +4,4 @@ const canal = "CFB Cursos";
 console.log(` ${nome}, ${canal} `);
 
 
-//interrompido em 7:24
+//interrompido em 7:24 https://www.youtube.com/watch?v=m6rkw8ITnBY&list=PLx4x_zx8csUhtPMrkiGvFJVE5LX8Qat5s&index=2
